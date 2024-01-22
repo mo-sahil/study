3e614c2f9dceb98001efa1e4491880319831b793
sahil <sahilbmu@gmail.com>
2024-01-22 09:47:44 +0530

    22 jan 2024

---
+++ R/dataTypeConversion.R
@@ -0,0 +1,4 @@
+a <- as.numeric(25L)
+a
+b <- as.numeric(2 + 3i)
+b
---
+++ R/inputclass.R
@@ -0,0 +1,6 @@
+name <- readline("Enter Your Name: ")
+print(name)
+age <- as.numeric(readline("Enter Your age: "))
+print(age)
+
+
