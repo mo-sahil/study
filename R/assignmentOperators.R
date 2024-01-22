2016f47fe045a541bb7902c23306d438a2b50af4
sahil <sahilbmu@gmail.com>
2024-01-22 10:51:37 +0530

    22 jan 2024

---
+++ R/assignmentOperators.R
@@ -0,0 +1,15 @@
+a= 10
+b <- 20
+30 -> c
+d <<-40
+50 ->> e
+x = c(a, b, c, d, e)
+if(10 %in% x){
+  print("Present")
+} else {
+  print("absent")
+}
+
+for(i in 1:15){
+  print(i)
+}
---
+++ R/multiply.R
@@ -0,0 +1,7 @@
+a <- as.numeric(readline("enter a: "))
+b <- as.numeric(readline("enter b: "))
+c <- as.numeric(readline("enter c: "))
+d <- as.numeric(readline("enter d: "))
+e <- as.numeric(readline("enter e: "))
+m <- a*b*c*d*e
+m
