75e94a7aa8bc332f1f4237b8650603fbcc40da6e
sahil <sahilbmu@gmail.com>
2024-01-24 11:37:04 +0530

    24 jan 2024

---
+++ R/elem_access.R
@@ -0,0 +1,10 @@
+names <- c("Sahil", "Maz", "Anas")
+names
+names[1]
+names[2]
+names[c(2,3)]
+names[-2]
+names
+d <- seq(from = 10, to = 100, by = 5)
+d
+d[c(-5,-8,-1)]
---
+++ R/vectors.R
@@ -0,0 +1,23 @@
+a <- c(1, 2, 3, 4)
+a
+
+b <-rep(a,c(1, 2, 3, 4))
+b
+
+c <- seq(1,5)
+c
+
+d <- 1:10
+d
+
+e <- seq(from = 10, to = 100, by = 5)
+e
+
+f <- c(names, d)
+f
+
+class(a)
+class(b)
+class(c)
+
+
