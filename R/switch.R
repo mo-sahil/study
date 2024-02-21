813fb7ae4231f13ded70a372306dcc92566b1def
Mo Sahil <sahilbmu@gmail.com>
2024-02-21 12:46:24 +0530

    21-feb-2024

---
+++ R/switch.R
@@ -0,0 +1,18 @@
+b = readline("Enter a number: ")
+c <- switch (b,
+  "1" = "Sahil",
+  "2" = "Maz",
+  "5" = "Ayush",
+  "10" = "Aban"
+)
+c
+
+a <- 2
+d <- switch(a, 
+  "Sahil",
+  "Maz",
+  "Ayush",
+  "Pawan",
+  "Aban"
+)
+d
--- java/ParseInt.java
+++ java/ParseInt.java
@@ -3,15 +3,13 @@ import java.util.*;
 class ParseInt{
     public static int decToBin(String n) {
         int dec = 0, i = n.length()-1, p = 0;
-        while(i >= 0) 
-            dec += (n.charAt(i--)-'0')*Math.pow(2,p++);
-        
+        while(i >= 0) dec += (n.charAt(i--)-'0')*Math.pow(2,p++);
         return dec;
     }
     public static void main(String args[]){
         String str = "100100", s = "111000";
         int n  = decToBin(str), m = decToBin(s);
         
-        System.out.println(n);
+        System.out.println(n^m);
     }
 }
