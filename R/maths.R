d42eeef8f85f2b98320ed0c517497197683b5079
Mo Sahil <sahilbmu@gmail.com>
2024-03-27 12:34:54 +0530

    27-march-2024

---
+++ R/maths.R
@@ -0,0 +1,31 @@
+#abs returns positive value
+x <- -4
+abs(x)
+
+#sqrt returns squareroot
+x <- 4
+sqrt(x)
+
+#ceiling returns the smallest integer which is larger than or equal to x.
+x <- 4.5
+ceiling(x)
+
+#floor returns the largest integer, which is smaller than or equal to x.
+x <- 3.5
+floor(x)
+
+#trunc returns truncated value
+x <- c(1.2, 3.4, 3.2)
+trunc(x)
+
+#round(x, digits = n) returns round value of input x
+x <- 2.3456
+round(x, digits = 3)
+
+x = 4
+cos(x)
+sin(x)
+tan(x)
+log(x)
+log10(x)
+exp(x)
