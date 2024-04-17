98f9b1269e3e575ce30b091ecfb12b51b341f3ea
Mo Sahil <sahilbmu@gmail.com>
2024-04-17 11:58:30 +0530

    17-April-2024

--- R/dplyr.R
+++ R/dplyr.R
@@ -1,4 +1,4 @@
-
+library(dplyr)
 
 df <- data.frame(
   name = c("Sahil", "Maz", "Anas"),
@@ -16,4 +16,11 @@ df
 select(df, 1)
 select(df, -starts_with("A"))
 
+df <- iris
+View(df)
 
+str(iris)
+
+df %>% select(Sepal.Length, Petal.Length, Species) %>% filter(Species == "setosa" & Petal.Length > 1.4 & Sepal.Length > 3.5)
+
+df %>% filter(Species == "setosa" & Petal.Length > 1.4 & Sepal.Length > 3.5) %>% mutate(SL = Sepal.Width) %>% select(Sepal.Length, Petal.Length, Species, SL)
