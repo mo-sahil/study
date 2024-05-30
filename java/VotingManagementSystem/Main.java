217d4a49ac66be07f457025982eab485f7e31790
Mo Sahil <sahilbmu@gmail.com>
2024-05-31 00:33:13 +0530

    Portfolio

--- java/VotingManagementSystem/Main.java
+++ java/VotingManagementSystem/Main.java
@@ -29,7 +29,7 @@ public class Main {
                         String candidateName = scanner.nextLine();
                         votingSystem.vote(user, candidateName);
                     }
-                    System.out.println("\n\n\n");
+                    System.out.println("\n\n");
                     break;
 
                 case 2:
@@ -38,30 +38,30 @@ public class Main {
                     System.out.print("Enter Password: ");
                     password = scanner.nextLine();
                     votingSystem.signUp(userId, password);
-                    System.out.println("\n\n\n");
+                    System.out.println("\n\n");
                     break;
 
                 case 3:
                     System.out.print("Enter candidate name: ");
                     String candidateName = scanner.nextLine();
                     votingSystem.addCandidate(candidateName);
-                    System.out.println("\n\n\n");
+                    System.out.println("\n\n");
                     break;
 
                 case 4:
                     votingSystem.showResults();
-                    System.out.println("\n\n\n");
+                    System.out.println("\n\n");
                     break;
 
                 case 5:
                     running = false;
                     System.out.println("Exiting the system. Goodbye!");
-                    System.out.println("\n\n\n");
+                    System.out.println("\n\n");
                     break;
 
                 default:
                     System.out.println("Invalid choice. Please try again.");
-                    System.out.println("\n\n\n");
+                    System.out.println("\n\n");
             }
         }
 
