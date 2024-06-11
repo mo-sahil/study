c7215887ae840829fca15d4003898ff6b22d6928
Mo Sahil <sahilbmu@gmail.com>
2024-06-11 11:27:26 +0530

    :

---
+++ SummerPep/dynamicAllocation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int* a = new int(10);
+    cout << endl << "a ---> " << a << endl << endl;
+    
+    cout << endl << "*a ---> " << *a << endl << endl;
+
+    int* arr = new int[10];
+    cout << endl << "arr ---> " << arr << endl << endl;
+    
+    cout << endl << "*arr ---> " << *arr << endl << endl;
+
+    char arr1[] = "Sahil";
+    cout << endl << "arr1 ---> " << arr1 << endl << endl;
+    
+    cout << endl << "*arr1 ---> " << *arr1 << endl << endl;
+
+}
