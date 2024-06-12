a12941f2e1e4765b59b34ff0be7c593b88b3dd33
Mo Sahil <sahilbmu@gmail.com>
2024-06-13 03:25:27 +0530

    dsa practice 13-june-2024

---
+++ dsa/learningPlan/13-06-2024/longestCommonPrefix.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string longestCommonPrefix(const vector<string>& strs) {
+    if (strs.empty()) {
+        return "";
+    }
+    
+    string prefix = strs[0];
+    
+    for (int i = 1; i < strs.size(); i++) {
+        while (strs[i].find(prefix) != 0) {
+            prefix = prefix.substr(0, prefix.length() - 1);
+            
+            if (prefix.empty()) {
+                return "";
+            }
+        }
+    }
+    
+    return prefix;
+}
+
+int main() {
+    vector<string> strings = {"Sahil", "Sahas", "Sithi"};
+    string commonPrefix = longestCommonPrefix(strings);
+    
+    cout << "Longest common prefix: " << commonPrefix << endl;
+    
+    return 0;
+}
